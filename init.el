51a5820f3c0bdf99d638d4dc335849a3257bfa5f
AzmainBiswas <azmainbiswas007@gmail.com>
2023-07-18 22:56:34 +0530

    Tue Jul 18 10:56:34 PM IST 2023

--- init.el
+++ init.el
@@ -11,7 +11,17 @@
 		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(package-selected-packages
    '(rainbow-mode all-the-icons-ivy-rich treemacs-magit treemacs-icons-dired treemacs-projectile imenu-list doom-modeline nerd-icons lua-mode auctex python-black elpy lsp-pyright company-box flycheck company lsp-treemacs lsp-ivy lsp-ui lsp-mode tree-sitter-langs tree-sitter yasnippet magit org-bullets smex ido-vertical-mode which-key counsel ivy-rich ivy vterm-toggle vterm projectile dashboard doom-themes catppuccin-theme rainbow-delimiters undo-tree general evil-collection evil dired-hide-dotfiles dired-open all-the-icons-dired dired-single auto-package-update use-package))
- '(warning-suppress-types '((use-package) (use-package))))
+ '(warning-suppress-types
+   '((use-package)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (use-package)
+     (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
